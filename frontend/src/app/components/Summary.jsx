804dd6da58b67f289407aabb7b05297cf82367dc
Cristhian Rodriguez <cristhian.rodriguez@farmatodo.com>
2019-09-28 15:28:04 -0500

    fix

--- frontend/src/app/components/Summary.jsx
+++ frontend/src/app/components/Summary.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ListGroup, Form } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
 
 class Summary extends Component {
   render() {
@@ -10,17 +10,11 @@ class Summary extends Component {
         <ListGroup.Item>Valor por distancia: $38.000</ListGroup.Item>
         <ListGroup.Item>Valor por tiempo $10.000</ListGroup.Item>
         <ListGroup.Item>Descuento: 8.000 (cupon 1 vez)</ListGroup.Item>
-        <ListGroup.Item variant="success">Total:40.000</ListGroup.Item>
-        <ListGroup.Item>
-          <Form.Group controlId="user">
-            <Form.Label htm>Medio de pago </Form.Label>
-            <Form.Control as="select">
-              <option>Efectivo</option>
-              <option>Tarjeta</option>
-              <option>PayPal</option>
-            </Form.Control>
-          </Form.Group>
-        </ListGroup.Item>
+        <br />
+        <ListGroup variant="flush">
+          <ListGroup.Item variant="success">Total:40.000</ListGroup.Item>
+          <ListGroup.Item variant="warning">Medio de pago</ListGroup.Item>
+        </ListGroup>
       </ListGroup>
     );
   }
