4d18c2b1cdfaab776a31d75a1b448a36ad0b538f
JulianGarzon22 <bryangarzon22@gmail.com>
2019-09-28 16:22:08 -0500

    Fix some styles on form and add map

--- frontend/src/app/components/BasicInfo.jsx
+++ frontend/src/app/components/BasicInfo.jsx
@@ -27,42 +27,76 @@ class BasicInfo extends Component {
   render() {
     return (
       <div>
-        <i class="fas fa-caret-down"></i>
+        <i className="fas fa-caret-down"></i>
         <h5 className="info_title">Información Básica</h5>
         <Form>
           <Form.Group controlId="user">
-            <Form.Label>Servicio para: </Form.Label>
-            <Form.Control as="select">
-              <option>Mí mismo</option>
-              <option>Hija - Andrea</option>
-              <option>Pareja - Sara</option>
-              <option>Amiga - Paola</option>
-            </Form.Control>
+            <div className="row justify-content-center align-self-center">
+              <div className="col-sm-3">
+                <Form.Label>Servicio para: </Form.Label>
+              </div>
+              <div className="col-sm-9">
+                <Form.Control as="select">
+                  <option>Mí mismo</option>
+                  <option>Hija - Andrea</option>
+                  <option>Pareja - Sara</option>
+                  <option>Amiga - Paola</option>
+                </Form.Control>
+              </div>
+            </div>
           </Form.Group>
 
           <Form.Group controlId="service">
-            <Form.Label>Tipo de servicio: </Form.Label>
-            <Form.Control as="select">
-              <option>Acompañamiento en ruta</option>
-              <option>Escolta</option>
-              <option>Vigilancia</option>
-            </Form.Control>
+            <div className="row">
+              <div className="col-sm-3">
+                <Form.Label>Tipo de servicio: </Form.Label>
+              </div>
+              <div className="col-sm-9">
+                <Form.Control as="select">
+                  <option>Acompañamiento en ruta</option>
+                  <option>Escolta</option>
+                  <option>Vigilancia</option>
+                </Form.Control>
+              </div>
+            </div>
           </Form.Group>
         </Form>
-        <label htmlFor="date">¿Cuándo? </label>
-        <DatePicker
-          controlId="id"
-          name="date"
-          value={this.state.date}
-          onChange={this.dateHandler}
-        />
-        <TimePicker onChange={this.timeHandler} value={this.state.time} />
-        <label htmlFor="initial">Punto de inicio: </label>
-        <Form.Control type="text" placeholder="Ej. CC. Américas" />{" "}
-        <i className="fas fa-search"></i>
-        <label htmlFor="initial">Punto final: </label>
-        <Form.Control type="text" placeholder="Ej. calle 26 #69a-20" />{" "}
-        <i className="fas fa-search"></i>
+        <div className="row my-1">
+          <div className="col-sm-3">
+            <label htmlFor="date">¿Cuándo? </label>
+          </div>
+          <div className="col-sm-9">
+            <DatePicker
+              controlId="id"
+              name="date"
+              value={this.state.date}
+              onChange={this.dateHandler}
+            />
+            <TimePicker onChange={this.timeHandler} value={this.state.time} />
+          </div>
+        </div>
+        <div className="row my-1">
+          <div className="col-sm-3">
+            <label htmlFor="initial">Punto de inicio: </label>
+          </div>
+          <div className="col-sm-8">
+            <Form.Control type="text" placeholder="Ej. CC. Américas" />
+          </div>
+          <div className="col-sm-1">
+            <i className="fas fa-search"></i>
+          </div>
+        </div>
+        <div className="row my-1">
+          <div className="col-sm-3">
+            <label htmlFor="initial">Punto final: </label>
+          </div>
+          <div className="col-sm-8">
+            <Form.Control type="text" placeholder="Ej. calle 26 #69a-20" />{" "}
+          </div>
+          <div className="col-sm-1">
+            <i className="fas fa-search"></i>
+          </div>
+        </div>
         <Form.Group controlId="user">
           <Row className="align-middle">
             <Col sm={2}>
