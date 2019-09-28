e123b3ad8bac4ef09a5f4efaca30684663d38a52
Cristhian Rodriguez <cristhian.rodriguez@farmatodo.com>
2019-09-28 17:58:05 -0500

    add

--- frontend/src/app/components/Summary.jsx
+++ frontend/src/app/components/Summary.jsx
@@ -1,33 +1,72 @@
 import React, { Component } from "react";
 import { ListGroup } from "react-bootstrap";
-import { Form } from "react-bootstrap";
+import { Form, Row, Col } from "react-bootstrap";
+const axios = require('axios');
+
+
 
 class Summary extends Component {
-  render() {
-    return (
-      <ListGroup variant="flush">
-        <ListGroup.Item>Distancia: 10km</ListGroup.Item>
-        <ListGroup.Item>Tiempo estimado: 45 minutos</ListGroup.Item>
-        <ListGroup.Item>Valor por distancia: $38.000</ListGroup.Item>
-        <ListGroup.Item>Valor por tiempo $10.000</ListGroup.Item>
-        <ListGroup.Item>Descuento: $8.000 (cupon 1 vez)</ListGroup.Item>
-        <br />
-        <ListGroup variant="flush">
-          <ListGroup.Item variant="success">Total: $40.000</ListGroup.Item>
-          <form>
-            <Form.Group controlId="user">
-              <Form.Label>Medio de pago: </Form.Label>
-              <Form.Control as="select">
-                <option>Efectivo</option>
-                <option>Tarjeta</option>
-                <option>PayPal</option>
-              </Form.Control>
-            </Form.Group>
-          </form>
-        </ListGroup>
-      </ListGroup>
-    );
-  }
+    constructor(props) {
+        super();
+        this.state = {
+            payMethods: [],
+            sumaryInfo: {}
+        };
+        const formatter = new Intl.NumberFormat('es-CO', {
+            style: 'currency',
+            currency: 'COP',
+            minimumFractionDigits: 0
+        })
+    }
+
+    async  componentDidMount(){
+
+        // await this.getUserData();
+        await this.getSummary();
+        // console.log("inicia");
+        //
+        // console.log(this.state.activeServices);
+    }
+
+    async getSummary(){
+        await axios.get(`http://us-central1-sentiments-co.cloudfunctions.net/dataEscoltApp?data=summary`)
+            .then(jsonResp => {
+                var ret = jsonResp.data.data;
+                // console.log(ret)
+                this.setState({sumaryInfo:ret})
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>Distancia: {this.state.sumaryInfo.distance}</ListGroup.Item>
+                    <ListGroup.Item>Tiempo estimado: {this.state.sumaryInfo.estimatedTime}</ListGroup.Item>
+                    <ListGroup.Item>Valor por distancia:  {new Intl.NumberFormat("es-CO", {style: "currency", currency: "COP"}).format(this.state.sumaryInfo.distanceValue)}</ListGroup.Item>
+                    <ListGroup.Item>Valor por tiempo:  {new Intl.NumberFormat("es-CO", {style: "currency", currency: "COP"}).format(this.state.sumaryInfo.timeValue)}</ListGroup.Item>
+                    <ListGroup.Item>Descuento:  {new Intl.NumberFormat("es-CO", {style: "currency", currency: "COP"}).format(this.state.sumaryInfo.discount)} (cupon 1 vez)</ListGroup.Item>
+                    <br />
+                    <ListGroup variant="flush">
+                        <ListGroup.Item variant="success">Total: {new Intl.NumberFormat("es-CO", {style: "currency", currency: "COP"}).format(this.state.sumaryInfo.invoiceValue)}</ListGroup.Item>
+                        <form>
+                            <Form.Group controlId="user">
+                                <Form.Label className="my-2">Medio de pago: </Form.Label>
+                                <Form.Control as="select">
+                                    <option>Efectivo</option>
+                                    <option>
+                                        Tarjeta de Credito (Visa XXXXXXXXXXX456)
+                                    </option>
+                                    <option>Tarjeta de Credito (Visa XXXXXXXXXXX321)</option>
+                                    <option>PayPal (dguti25@gmail.com)</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </form>
+                    </ListGroup>
+                </ListGroup>
+            </div>
+        );
+    }
 }
 
 export { Summary };
